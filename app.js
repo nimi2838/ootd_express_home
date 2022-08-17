9bdd612c49181942ddf1d6f8032984a9ef893748
nimi2838 <bbongsky11@naver.com>
2022-08-17 18:34:16 +0900

    회원가입 하는 중

--- app.js
+++ app.js
@@ -2,29 +2,51 @@ import express from "express";
 import mysql from "mysql2/promise";
 import cors from "cors";
 
-
 const app = express();
 app.use(cors());
 
 const port = 4000;
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "team",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "team",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-app.get('/test1', async (req, res) => {
+app.get("/test1", async (req, res) => {
   const [rows] = await pool.query(
     `
   SELECT *
   FROM user 
-  `);
-  
+  `
+  );
+
   res.json(rows);
+});
+
+app.post("/test1", async (req, res) => {
+  const {
+    body: { id, pw, name },
+  } = req;
+  console.log(id, pw, name);
+
+  await pool.query(
+    `
+    INSERT INTO user
+    SET id = ?, pw = ?, name =?
+    `
+  );
+  // const [rows] = await pool.query(
+  //   `
+  // SELECT *
+  // FROM user
+  // `
+  // );
+
+  // res.json(rows);
 });
 
 // app.post('/user', async (req, res) => {
@@ -34,7 +56,6 @@ app.get('/test1', async (req, res) => {
 //   )
 // })
 
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
