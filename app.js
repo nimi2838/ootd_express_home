83fcad54736c4077805186371c173aa7171ac653
nimi2838 <bbongsky11@naver.com>
2022-09-21 18:48:05 +0900

    관심상품 수정 중

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.post("/test1/doLogin", async (req, res) => {
   `,
     [id, pw]
   );
+  // console.log(userRow);
   if (userRow) {
     return res.send(true);
   }
@@ -66,6 +67,7 @@ app.post("/test1", async (req, res) => {
     body: { id, pw, name },
   } = req;
 
+  // console.log(id, pw, name);
   // const { text } = req.body;
 
   const [row] = await pool.query(
@@ -90,7 +92,9 @@ app.post("/prdlist", async (req, res) => {
   const {
     body: { prdno },
   } = req;
+  // console.log("prdno", prdno);
   var like = "%" + prdno + "%";
+  // console.log("like", like);
 
   const [prdRow] = await pool.query(
     `
@@ -102,12 +106,14 @@ app.post("/prdlist", async (req, res) => {
   );
 
   res.json(prdRow);
+  // console.log(prdRow);
 });
 
 app.post("/product", async (req, res) => {
   const {
     body: { prdId },
   } = req;
+  // console.log("prdId", prdId);
 
   const [[prdRow]] = await pool.query(
     `
@@ -123,16 +129,16 @@ app.post("/product", async (req, res) => {
 
 app.post("/cart", async (req, res) => {
   const {
-    body: { prdId, userId },
+    body: { prdId },
   } = req;
 
   const [duplicate] = await pool.query(
     `
     SELECT *
-    FROM cart
-    WHERE prdId =? and userId = ?
+    FROM product
+    WHERE prdId =?
     `,
-    [prdId, userId]
+    [prdId]
   );
 
   const [[product]] = await pool.query(
@@ -143,6 +149,8 @@ app.post("/cart", async (req, res) => {
     `,
     [prdId]
   );
+
+  console.log(product.prdPrice);
 
   if (duplicate.length == 0) {
     const [row] = await pool.query(
@@ -183,6 +191,10 @@ app.patch("/amount/:prdId", async (req, res) => {
     body: { count, price },
   } = req;
 
+  console.log("count", req.body.count);
+
+  console.log("price", req.body.price);
+
   await pool.query(
     `
     UPDATE cart SET amount = ?, price = ? WHERE prdId = ?
@@ -208,6 +220,8 @@ app.post("/cartList2", async (req, res) => {
     body: { prdId },
   } = req;
 
+  // console.log("prdId", prdId);
+
   const [[cartRow]] = await pool.query(
     `
     SELECT *
@@ -218,6 +232,25 @@ app.post("/cartList2", async (req, res) => {
   );
 
   res.json(cartRow);
+  // console.log("cartRow", cartRow);
+});
+app.post("/SearchPage", async (req, res) => {
+  const {
+    body: { search },
+  } = req;
+  var like = "%" + search + "%";
+  const [prdLow] = await pool.query(
+    `
+    SELECT *
+    FROM product
+    WHERE prdName LIKE ?
+    `,
+    [like]
+  );
+  console.log(prdLow);
+  if (search) {
+    res.json(prdLow);
+  }
 });
 
 app.post("/totalPrice", async (req, res) => {
@@ -237,6 +270,9 @@ app.post("/totalPrice", async (req, res) => {
 app.patch("/check/:userId/:prdId", async (req, res) => {
   const { userId, prdId } = req.params;
 
+  console.log("userId", userId);
+
+  console.log("prdId", prdId);
   const [[rows]] = await pool.query(
     `
     SELECT *
@@ -255,64 +291,245 @@ app.patch("/check/:userId/:prdId", async (req, res) => {
   );
   res.send(userId);
 });
+// ↓↓ 공지사항 데이터 ↓↓
 
-app.post("/cartDelete", async (req, res) => {
+app.get("/notices", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM Notice ORDER BY id DESC");
+
+  res.json(rows);
+});
+
+app.post("/notices", async (req, res) => {
   const {
-    body: { prdId, userId },
+    body: { text },
   } = req;
-
-  const [[cartRow]] = await pool.query(
+  await pool.query(
     `
-    SELECT *
-    FROM cart
-    WHERE userId = ? and prdId = ?
-    `,
-    [userId, prdId]
-  );
-
-  if (cartRow.length != 0) {
-    await pool.query(
-      `
-    DELETE FROM cart 
-    WHERE userId = ? AND prdId = ?;
+  INSERT INTO Notice
+  SET reg_date = NOW(),
+  perform_date = '2022-05-18 07:00:00',
+  checked = 0,
+  text = ?;
   `,
-
-      [userId, prdId]
-    );
-  }
-
-  const [cartDelete] = await pool.query(
-    `
-    SELECT *
-    FROM cart
-    WHERE userId = ? 
-    `,
-    [userId]
+    [text]
   );
-  // console.log(cartDelete);
-
-  res.json(cartDelete);
+  const [newRows] = await pool.query(`
+  SELECT *
+  FROM Notice
+  ORDER BY id
+  DESC
+  `);
+  res.json(newRows);
 });
 
-app.post("/SearchPage", async (req, res) => {
+app.get("/notices/:id/", async (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+
+  const [rows] = await pool.query(
+    `
+  SELECT *
+  FROM Notice
+  WHERE id = ?
+  `,
+    [id]
+  );
+  if (rows.length === 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  res.json(rows[0]);
+});
+
+app.patch("/notices/:id", async (req, res) => {
+  const { id } = req.params;
+  const { perform_date, text } = req.body;
+
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM Notice
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  if (rows.length === 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+  }
+
+  if (!perform_date) {
+    res.status(400).json({
+      msg: "perform_date required",
+    });
+    return;
+  }
+
+  if (!text) {
+    res.status(400).json({
+      msg: "text required",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
+    UPDATE Notice
+    SET perform_date = ?,
+    text = ?,
+    WHERE id = ?
+    `,
+    [perform_date, text, id]
+  );
+
+  const [updatednotices] = await pool.query(
+    `
+    SELECT *
+    FROM Notice
+    ORDER BY id DESC
+    `
+  );
+  res.json(updatednotices);
+});
+
+app.patch("/notices/check/:id", async (req, res) => {
+  const { id } = req.params;
+  //id번 Notice가 없을 수도 있기 때문에
+  //SELECT * FROM으로 id값을 불러옴 → id가 없는 값을 불러온다면?
+  //if (!rows) 로 404에러를 할당하며 msg: "not found" 출력.
+  //또한, check 초기상태를 파악하기 위해 불러와야 함.
+  const [[rows]] = await pool.query(
+    `
+    SELECT *
+  FROM Notice WHERE id = ?
+  `,
+    [id]
+  );
+  if (!rows) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+  //문제가 없다면? → 수정 mySQL 넣기
+  await pool.query(
+    `
+  UPDATE Notice
+  SET checked = ?
+  WHERE id = ?
+  `,
+    //check값은 어떻게 바꾸나?
+    //위에서 받은 초기값의 반전값 (0이면 1, 1이면 0)을 반영시켜줌.
+    [!rows.checked, id]
+  );
+  //값을 바꾼 후, 바꾼 값을 저장한 새로운 테이블을 다시 보여 줌.
+  const [updatedNotice] = await pool.query(
+    `
+      SELECT * FROM Notice ORDER BY id DESC`,
+    [id]
+  );
+  //반환시켜줌
+  res.json(updatedNotice);
+  //res.send(id);
+});
+
+app.delete("/notices/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const [[NoticeRow]] = await pool.query(
+    `
+    SELECT *
+    FROM Notice
+    WHERE id = ?`,
+    [id]
+  );
+
+  if (NoticeRow === undefined) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `DELETE FROM Notice
+    WHERE id = ?`,
+    [id]
+  );
+  res.json({
+    msg: `${id}번 공지사항이 삭제되었습니다.`,
+  });
+});
+app.post("/SBP", async (req, res) => {
+  const { prdId } = req.body;
+
+  const [[postItem]] = await pool.query(
+    `
+    SELECT * FROM product
+    WHERE prdId = ?
+    `,
+    [prdId]
+  );
+  const id = postItem.prdId;
+  await pool.query(
+    `
+    INSERT INTO cart
+    SET prdId = ?,
+    prdName = ?,
+    prdEname = ?,
+    prdPrice = ?,
+    prdImg = ?
+    `,
+    [
+      id,
+      postItem.prdName,
+      postItem.prdEname,
+      postItem.prdPrice,
+      postItem.prdImg,
+    ]
+  );
+
+  const [CartRow] = await pool.query(
+    `
+    SELECT * FROM cart
+    WHERE prdId = ?
+    `,
+    [id]
+  );
+});
+app.get("/SBP", async (req, res) => {
   const {
-    body: { search },
+    body: { prdId },
   } = req;
-  var like = "%" + search + "%";
-  const [prdLow] = await pool.query(
+  const [ItemList] = await pool.query(
     `
-    SELECT *
-    FROM product
-    WHERE prdName LIKE ?
+    SELECT * FROM 
+    cart 
     `,
-    [like]
+    [prdId]
   );
-  // console.log(prdLow);
-  if (search) {
-    res.json(prdLow);
-  }
+  res.json(ItemList);
 });
 
+// const {abc} = await pool.query(`
+// select * from follow_table where followid = ? and followedid = ?`,[a,b])
+// if(abc==null){
+//   팔로우를해라
+//   나의팔로우+1
+//   하늘님팔로워+1
+//   follow_table 내아이디와 하늘님아이디
+// }else {
+//   팔로을 추ㅣ소해라
+//   나의팔로우-1
+//   하늘님팔로워-1
+//   delete from follow_table where followid = ? and followedid = ?`,[a,b])
+//   follow_table 내아이디와 하늘님아이디 삭제.
+// }
 app.patch("/addHeart", async (req, res) => {
   const {
     body: { prdId, userId, checked },
@@ -389,3 +606,5 @@ app.post("/HeartCount", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// ↓↓ 공지사항 데이터 ↓↓
